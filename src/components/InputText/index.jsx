3bb799d7c0d3aab496245ab997cae67d1fc0218e
Gabriel Paiva <gabriel.paiva@pipefy.com>
2022-11-07 21:27:15 -0300

    feat: install react hook form and integrate with login

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -11,8 +11,7 @@ export const InputText = memo(
     marginRight,
     marginBottom,
     marginTop,
-    showPasswordIconVisibility,
-    icon,
+    secureTextEntry = false,
     placeholder,
     ...rest
   }) => {
@@ -23,42 +22,23 @@ export const InputText = memo(
         marginRight={marginRight}
         marginBottom={marginBottom}
         marginTop={marginTop}
-        showPasswordIconVisibility={showPasswordIconVisibility}
         style={{ borderBottomWidth: 2, borderBottomColor: colors.orange }}
       >
-        {icon && showPasswordIconVisibility ? (
-          <>
-            <TextInput
-              placeholderTextColor={colors.black}
-              placeholder={placeholder}
-              style={{
-                color: colors.black,
-                width: '80%',
-                paddingLeft: 18,
-                paddingRight: 18,
-                paddingTop: 18,
-                paddingBottom: 18,
-              }}
-              {...rest}
-            />
-            {icon}
-          </>
-        ) : (
-          <TextInput
-            placeholder={placeholder}
-            placeholderTextColor="#2A2C4D50"
-            style={{
-              color: colors.black,
-              width: '100%',
-              paddingLeft: 18,
-              paddingRight: 18,
-              paddingTop: 18,
-              paddingBottom: 18,
-              outlineStyle: 'none',
-            }}
-            {...rest}
-          />
-        )}
+        <TextInput
+          placeholder={placeholder}
+          placeholderTextColor="#2A2C4D50"
+          secureTextEntry={secureTextEntry}
+          style={{
+            color: colors.black,
+            width: '100%',
+            paddingLeft: 18,
+            paddingRight: 18,
+            paddingTop: 18,
+            paddingBottom: 18,
+            outlineStyle: 'none',
+          }}
+          {...rest}
+        />
       </Container>
     );
   }
--- src/screens/Guests/Login/index.jsx
+++ src/screens/Guests/Login/index.jsx
@@ -1,14 +1,58 @@
 import { Container } from './styles';
 import { Button, Text, InputText, Header } from '../../../components';
+import { useForm, Controller } from 'react-hook-form';
+import { useAuth } from '../../../hooks/auth.jsx';
 
 export const Login = () => {
+  const { user, signIn } = useAuth();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = async (data) => {
+    console.log(data);
+    await signIn(data);
+  };
+
   return (
     <Container>
       <Header pageName="Login" />
-      <InputText placeholder="E-mail" />
-      <InputText placeholder="Senha" showPasswordIconVisibility />
-      <InputText placeholder="Confirmar a senha" showPasswordIconVisibility />
-      <Button textContent="Entrar" isBordered />
+      <Controller
+        control={control}
+        name="email"
+        // rules={{
+        //   required: 'Informe um e-mail',
+        //   pattern: {
+        //     value: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i,
+        //     message: 'E-mail Inválido',
+        //   },
+        // }}
+        render={({ field: { onChange } }) => (
+          <InputText placeholder="E-mail" onChangeText={onChange} />
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        // rules={{
+        //   required: 'Informe uma senha',
+        // }}
+        render={({ field: { onChange } }) => (
+          <InputText
+            placeholder="Senha"
+            onChangeText={onChange}
+            secureTextEntry
+          />
+        )}
+      />
+      <Button
+        textContent="Entrar"
+        isBordered
+        onPress={handleSubmit(handleLogin)}
+      />
     </Container>
   );
 };
